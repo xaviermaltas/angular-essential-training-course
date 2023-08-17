b9a7e5b1db27c9e08a81220d176ffb0ada88020c
Xavier Maltas <xavi.maltass@gmail.com>
2023-08-17 17:13:30 +0200

    02_07

---
+++ src/app/media-item.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'media-item',
+  templateUrl: './media-item.component.html',
+  styleUrls: ['./media-item.component.css']
+})
+export class MediaItemComponent {}
