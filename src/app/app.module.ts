2219fe1ac7967e0326c63521e396ccf6095171d2
Xavier Maltas <xavi.maltass@gmail.com>
2023-08-21 15:47:50 +0200

    03_08 custom pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,26 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { MediaItemComponent } from "./media-item.component";
+import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
-import { FavoriteDirective } from "./favorite.directive";
-import { CategoryListPipe } from "./category-list.pipe";
+import { FavoriteDirective } from './favorite.directive';
+import { CategoryListPipe } from './category-list.pipe';
+import { MediaItemFormComponent } from './media-item-form.component';
 
 @NgModule({
-    imports :[
-        BrowserModule
-    ],
-    declarations: [
-        AppComponent,
-        MediaItemComponent,
-        MediaItemListComponent,
-        FavoriteDirective,
-        CategoryListPipe
-    ],
-    bootstrap: [
-        AppComponent
-    ]
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    AppComponent,
+    MediaItemComponent,
+    MediaItemListComponent,
+    FavoriteDirective,
+    CategoryListPipe,
+    MediaItemFormComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
-
-export class AppModule{}
+export class AppModule {}
