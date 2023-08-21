489d319cf1899e2e6f4f529e7bb55927429f94d1
Xavier Maltas <xavi.maltass@gmail.com>
2023-08-21 17:06:54 +0200

    04_03 model-driven forms

--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'mw-media-item-form',
   templateUrl: './media-item-form.component.html',
   styleUrls: ['./media-item-form.component.css']
 })
-export class MediaItemFormComponent {}
+export class MediaItemFormComponent implements OnInit {
+  form: FormGroup;
+  // ngOnInit(): void {
+  //   this.form = new FormGroup();
+  // }
+  ngOnInit(){
+    this.form = new FormGroup({
+      medium: new FormControl('Movies'),
+      name: new FormControl(''),
+      category: new FormControl(''),
+      year: new FormControl(''),
+    });
+  }
+
+  onSubmit(mediaItem) {
+    console.log(mediaItem);
+  }
+
+
+}
