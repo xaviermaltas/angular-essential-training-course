dd08447dd3390fdea853f363f918a78320e079d7
Xavier Maltas <xavi.maltass@gmail.com>
2023-08-28 19:07:18 +0200

    08_05 - global css styles file

--- src/app/category-list.component.ts
+++ src/app/category-list.component.ts
@@ -24,14 +24,6 @@ import { Component, Input } from '@angular/core';
         margin-right: 4px;
         margin-bottom: 4px;
       }
-      .label {
-        background-color: #999999;
-        color: #ffffff;
-        border-radius: 4px;
-        padding: 2px 6px;
-        font-style: italic;
-        width: max-content;
-      }
     `
   ]
 })
