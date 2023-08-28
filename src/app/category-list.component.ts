afedacb8e092a2f9eb57fc077613bb46ea9935bb
Xavier Maltas <xavi.maltass@gmail.com>
2023-08-28 19:02:51 +0200

    08_04 - css selector with atribute selector

--- src/app/category-list.component.ts
+++ src/app/category-list.component.ts
@@ -9,6 +9,16 @@ import { Component, Input } from '@angular/core';
   `,
   styles: [
     `
+      :host {
+        display: block;
+        margin-bottom: 20px;
+      }
+      :host-context(.medium-movies) span {
+        background-color: #53ace4;
+      }
+      :host-context(.medium-series) span {
+        background-color: #45bf94;
+      }
       span {
         display: inline-block;
         margin-right: 4px;
