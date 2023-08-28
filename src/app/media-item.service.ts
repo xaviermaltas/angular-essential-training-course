71a131d7575397d46b4f5e0e6b553dbfeb9a352d
Xavier Maltas <xavi.maltass@gmail.com>
2023-08-28 18:54:58 +0200

    test

--- src/app/media-item.service.ts
+++ src/app/media-item.service.ts
@@ -1,45 +1,48 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators';
-import { throwError } from "rxjs";
+import { throwError } from 'rxjs';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
-
 export class MediaItemService {
-    constructor( private http: HttpClient){}
+  constructor(private http: HttpClient) {}
 
-    get(medium){
-      const getOptions = {
-        params: { medium }
-      };
-      return this.http.get<MediaItemResponse>('mediaitems', getOptions)
-        .pipe(
-          map((response: MediaItemResponse ) => {
-            return response.mediaItems;
-          }),
-          catchError(this.handleError)
-        );
-    }
+  get(medium: string) {
+    const getOptions = {
+      params: { medium }
+    };
+    return this.http.get<MediaItemsResponse>('mediaitems', getOptions)
+      .pipe(
+        map((response: MediaItemsResponse) => {
+          return response.mediaItems;
+        }),
+        catchError(this.handleError)
+      );
+  }
 
-    add(mediaItem){
-      return this.http.post('mediaitems', mediaItem)
-        .pipe(catchError(this.handleError));
-    }
+  add(mediaItem: MediaItem) {
+    return this.http.post('mediaitems', mediaItem)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
 
-    delete(mediaItem){
-      return this.http.delete(`mediaitems/${mediaItem.id}`)
-        .pipe(catchError(this.handleError));
-    }
+  delete(mediaItem: MediaItem) {
+    return this.http.delete(`mediaitems/${mediaItem.id}`)
+    .pipe(
+      catchError(this.handleError)
+    );
+  }
 
-    private handleError(error: HttpErrorResponse){
-      console.log(error.message);
-      return throwError('A data error occured, please try again' + error);
-    }
+  private handleError(error: HttpErrorResponse) {
+    console.error(error.message);
+    return throwError('A data error occurred, please try again.');
+  }
 }
 
-interface MediaItem {
+export interface MediaItem {
   id: number;
   name: string;
   medium: string;
@@ -49,6 +52,6 @@ interface MediaItem {
   isFavorite: boolean;
 }
 
-interface MediaItemResponse {
+interface MediaItemsResponse {
   mediaItems: MediaItem[];
 }
